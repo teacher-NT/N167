5c6825dadb61e071a7415521ab9482fd4b7d99ae
holmon-alp <alpholmon@gmail.com>
2025-07-24 11:30:52 +0500

    Method

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import os
 os.system("cls")
 
-lst = ('Salom', 'hayr', 'tugadi')
-# print(lst.index('b'))
-# print(lst.count('b'))
+lst = [8,6,3,25,1,4,2,5,7]
+
+# lst.append(11)
+# lst.append(['a','b','c'])
+# print(lst)
+
+# lst.insert(4,"salom")
+# print(lst)
+
+# lst2 = [99,88,77]
+# lst.extend(lst2)
+# print(lst)
+
+# lst.remove(25)
+# print(lst)
+
+# a = lst.pop(3)
+# print(a)
+# print(lst)
+
+# lst.clear()
+# print(lst)
+
+# lst.sort(reverse=True)
+# print(lst)
+
+# lst2 = lst.copy()
+# lst2 = lst
+# lst2[2] = "Bugun sening kuning"
+# print(lst2, lst, sep='\n')
+
+
