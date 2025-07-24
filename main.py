933b6bfbd9d916944301dfe94f9551d86da25f34
holmon-alp <alpholmon@gmail.com>
2025-07-24 10:04:20 +0500

    Tuple

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
 os.system("cls")
 
-my_tuple = ('apple', 'google', 'tesla', 'meta', 'nasa')
+# my_tuple = ('apple', 'google', 'tesla', 'meta', 'nasa')
 
-# if 'tesla' in my_tuple:
-#     print('Tesla bor')
-# else:
-#     print(my_tuple)
+# tuple2 = ('samsung', 'amazon', 'microsoft')
 
-# for i in my_tuple:
-#     print(i)
+# tuple3 = my_tuple + tuple2
+
+# print(tuple3)
+
+tuple2 = ('samsung', 'amazon', 'microsoft', 'sony')
+
+# a = tuple2[0]
+# b = tuple2[1]
+# c = tuple2[2]
+# a,b,c = tuple2
+
+# a,b,*c = tuple2
+
+# a,b,c,d,e,f = tuple2
+
+# print(a,b,c)
