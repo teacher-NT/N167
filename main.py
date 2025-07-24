30d2c640bd29ee8824e50c9cacd116e4c780804c
holmon-alp <alpholmon@gmail.com>
2025-07-24 10:12:05 +0500

    Tuple

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import os
 os.system("cls")
 
-# my_tuple = ('apple', 'google', 'tesla', 'meta', 'nasa')
+my_tuple = ('apple', 'google', 'tesla', 'meta', 'nasa', 1,2,3,4,5)
 
-# tuple2 = ('samsung', 'amazon', 'microsoft')
+print(len(my_tuple))
 
-# tuple3 = my_tuple + tuple2
-
-# print(tuple3)
-
-tuple2 = ('samsung', 'amazon', 'microsoft', 'sony')
-
-# a = tuple2[0]
-# b = tuple2[1]
-# c = tuple2[2]
-# a,b,c = tuple2
-
-# a,b,*c = tuple2
-
-# a,b,c,d,e,f = tuple2
-
-# print(a,b,c)
+matn = 'Bugun havo kechagidan issiq'
+print(len(matn))
