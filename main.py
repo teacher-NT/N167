f7e61ff064c0bdf02ca2cf39a04aa8a167532008
holmon-alp <alpholmon@gmail.com>
2025-07-24 11:09:08 +0500

    Method

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os
 os.system("cls")
 
-lst = [1,2,3,4,5,6,7]
-
-lst[4] = 55
-print(lst)
-# print(lst[3])
-# print(lst[:5])
-# print(lst[2:])
+lst = ('Salom', 'hayr', 'tugadi')
+# print(lst.index('b'))
+# print(lst.count('b'))
