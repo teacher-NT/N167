671928c984afba63fec82eeeaccb12ae28b68352
holmon-alp <alpholmon@gmail.com>
2025-07-29 09:52:25 +0500

    Dict

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 import os
 os.system("cls")
 
-myset = {4,7,2,1,3,5,6,8,9,10}
-myset2 = {4,5,6,7,8,9}
+car = {
+    "brand": "BMW",
+    "model": "M8",
+    "price": 215000,
+    "speed": 330,
+    "mileage": 0,
+    "year": 2023,
+}
 
-# myset.update(myset2)
-# myset3 = myset.union(myset2)
+# print(car['brant'])
+# print(car.get('bran', f"Xato kalit, {car.keys()}"))
 
-# myset3 = myset.intersection(myset2)
-# myset.intersection_update(myset2)
+# print(car.keys())
 
-# myset3 = myset.difference(myset2)
-# myset.difference_update(myset2)
+# print(car.values())
 
-# myset3 = myset.symmetric_difference(myset2)
-# myset.symmetric_difference_update(myset2)
+# car.pop('year')
+# print(car)
 
-# print(myset2.issubset(myset))
+# print(car.items())
+# for k,q in car.items():
+#     print(k, q)
 
-# print(myset2.issuperset(myset))
+# car.popitem()
+# print(car)
 
-# print(myset3)
-# print(myset)
-
-
-# myset = {1,0,True,False}
-# print(myset)
-
-
-
-
-
-
-
-
-
-
-
-
-# print(myset[2])
-# myset[1] = 15
+# car['price'] = 88000
+# car["mileage"] = 4000
+# car['color'] = 'red'
+car.update({"price": 88000, 'mileage':4000, 'color':'red'})
+print(car)
