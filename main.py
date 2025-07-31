77ee0f121698d34299ac3060a86876ca0a741b90
holmon-alp <alpholmon@gmail.com>
2025-07-31 11:02:42 +0500

    Lambda

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
 os.system("cls")
 
-def salom_ber():
-    print("Salom hammaga")
+# def func(*a):
+#     print(sum(a))
 
-def func(a,b,c):
-    d = a+b+c
-    e = (a+b+c)/3
-    f = a*b*c
-    h = a-b-c
-    return d, e, f, h
+# func(1,2,3,4,5,6,7,8,9)
 
-*n, m = func(5,6,7)
-print(n, m)
+# def func2(**person):
+#     print(person['ism'])
 
+# func2(ism='Jamshid', yosh=23, manzil='Sergili')
+
+
+# def func3(a,b):
+#     return a+b
+
+func3 = lambda a,b : a+b
+
+print(func3(4,6))
