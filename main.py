23ab47aec9617059ff3c3af82d0f7c7aa1544967
holmon-alp <alpholmon@gmail.com>
2025-08-09 08:54:10 +0500

    Binary file

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 import os
 os.system("cls")
 
-import cv2
-import cv2.data
-
-model = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-camera = cv2.VideoCapture(0)
-while True:
-    is_valid, image = camera.read()
-    if is_valid:
-        faces = model.detectMultiScale(image, 1.1, 4, minSize=(30,30))
-        for face in faces:
-            x,y,w,h = face
-            cv2.rectangle(image, (x,y), (x+w, y+h), color=(12,27,240), thickness=2)
-        cv2.imshow('Kamera', image)
-    else:
-        print("Kamerada nosozlik")
-
-    if cv2.waitKey(1) & 0xFF == 32:
-        break
+with open("N167/image.jpeg", "rb") as file:
+    data = file.read()
+    print(len(list(data)))
+    # for i in list(data):
+    #     print(i, end=" ")
