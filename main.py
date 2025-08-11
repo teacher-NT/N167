e282fcc3e392c2afc3ac49176592268e49009fc8
holmon-alp <alpholmon@gmail.com>
2025-08-11 18:54:49 +0500

    test

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 os.system("cls")
 
-with open("N167/image.jpeg", "rb") as file:
-    data = file.read()
-    print(len(list(data)))
-    # for i in list(data):
-    #     print(i, end=" ")
+
+f = {
+    "olma": [13000, 14000, 15000],
+    "anor": [19000, 22000, 24000, 15000],
+    "gilos": [6000, 9000, 5000, 4000],
+    "banan": [30000, 28000]
+}
+
+for i in f:
+    print(f"{i}:", int(sum(f[i])/len(f[i])))
